1975fe2916d4c965c0375242a2bbefef69eb7ff5
HunggTraan <39126680+HunggTraan@users.noreply.github.com>
2020-10-12 13:12:20 +0700

    Login and Register

---
+++ eShopSolution.Application/System/Users/IUserService.cs
@@ -0,0 +1,12 @@
+﻿using eShopSolution.ViewModels.System.Users;
+using System.Threading.Tasks;
+
+namespace eShopSolution.Application.System.Users
+{
+    public interface IUserService
+    {
+        Task<string> Authenticate(LoginRequest request);
+
+        Task<bool> Register(RegisterRequest request);
+    }
+}
