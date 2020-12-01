bbd459334e1b8a8b5374373907795724fad57da7
HunggTraan <39126680+HunggTraan@users.noreply.github.com>
2020-12-01 10:48:55 +0700

    minor change

--- eShopSolution.AdminApp/Controllers/HomeController.cs
+++ eShopSolution.AdminApp/Controllers/HomeController.cs
@@ -1,14 +1,9 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Diagnostics;
-using System.Linq;
-using System.Threading.Tasks;
-using Microsoft.AspNetCore.Mvc;
-using Microsoft.Extensions.Logging;
-using eShopSolution.AdminApp.Models;
-using Microsoft.AspNetCore.Authorization;
+﻿using eShopSolution.AdminApp.Models;
 using eShopSolution.Utilities.Constants;
 using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System.Diagnostics;
 
 namespace eShopSolution.AdminApp.Controllers
 {
